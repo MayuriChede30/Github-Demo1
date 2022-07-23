bf53e756ab0553e72f0fc2163d897d440037802c
MayuriChede30 <109840064+MayuriChede30@users.noreply.github.com>
2022-07-22 21:58:07 -0700

    upadate in additon of two numbers in python

--- Addition.py
+++ Addition.py
@@ -1,6 +1,11 @@
+#Program to Find addtion of two numbers in python
 x = input("Type a number: ")
 y = input("Type another number: ")
 
 sum = int(x) + int(y)
 
 print("The sum is: ", sum)
+#output:-
+#Type a number:3
+#Type another number:5
+#The sum is:8
